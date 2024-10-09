ea422d32875b9effaa496cbfcd959030ea14d50d
jlindhorst <jlindhorst@letmeship.com>
2024-10-09 14:10:41 +0200

    improve exception handling

--- src/main/kotlin/jan/challenge/boudary/ControllerExceptionHandler.kt
+++ src/main/kotlin/jan/challenge/boudary/ControllerExceptionHandler.kt
@@ -2,6 +2,7 @@ package jan.challenge.boudary
 
 import jan.challenge.exceptions.ProductNotFoundException
 import jan.challenge.exceptions.ProductNotValidException
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
@@ -9,15 +10,30 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 
 @ControllerAdvice
 class ControllerExceptionHandler {
+    companion object {
+        private val logger = LoggerFactory.getLogger(ProductController::class.java)
+    }
 
     @ExceptionHandler
     fun handleProductNotValidException(ex: ProductNotValidException): ResponseEntity<String> {
+        logError(ex.message)
         return ResponseEntity(ex.message, HttpStatus.BAD_REQUEST)
     }
 
     @ExceptionHandler
     fun handleProductNotFound(ex: ProductNotFoundException): ResponseEntity<String> {
-        return ResponseEntity("product with id ${ex.productId} was not found", HttpStatus.BAD_REQUEST)
+        logError(ex.message)
+        return ResponseEntity(ex.message, HttpStatus.BAD_REQUEST)
+    }
+
+    @ExceptionHandler
+    fun handleException(ex: Exception): ResponseEntity<String> {
+        logger.error("Exception occurred: ${ex.message}", ex)
+        return ResponseEntity("An error occurred: ${ex.message}", HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+
+    private fun logError(exceptionMessage: String?) {
+        logger.error("Exception occurred: $exceptionMessage")
     }
 
 }
--- src/main/kotlin/jan/challenge/exceptions/ProductNotFoundException.kt
+++ src/main/kotlin/jan/challenge/exceptions/ProductNotFoundException.kt
@@ -1,3 +1,3 @@
 package jan.challenge.exceptions
 
-class ProductNotFoundException(val productId: Long) : Exception()
+class ProductNotFoundException(val productId: Long) : Exception("product with id $productId was not found")
