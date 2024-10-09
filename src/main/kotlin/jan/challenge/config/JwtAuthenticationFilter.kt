39f1261c0fcb377a9f2dcab27f7aa2746d80ba84
jlindhorst <jlindhorst@letmeship.com>
2024-10-09 22:53:12 +0200

    return a 403 with message when using wrong JWT token instead of 500

--- src/main/kotlin/jan/challenge/config/JwtAuthenticationFilter.kt
+++ src/main/kotlin/jan/challenge/config/JwtAuthenticationFilter.kt
@@ -5,36 +5,52 @@ import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import jan.challenge.service.CustomUserDetailsService
 import jan.challenge.service.TokenService
+import org.slf4j.LoggerFactory
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
-
 @Component
 class JwtAuthenticationFilter(
     private val userDetailsService: CustomUserDetailsService,
     private val tokenService: TokenService,
 ) : OncePerRequestFilter() {
+    val log = LoggerFactory.getLogger(JwtAuthenticationFilter::class.java)
+
     override fun doFilterInternal(
         request: HttpServletRequest,
         response: HttpServletResponse,
         filterChain: FilterChain
     ) {
         val authHeader: String? = request.getHeader("Authorization")
-        if (authHeader.doesNotContainBearerToken()) {
-            filterChain.doFilter(request, response)
+
+        if (authHeader == null || authHeader.doesNotContainBearerToken()) {
+            return filterChain.doFilter(request, response)
+        }
+
+        val jwtToken = authHeader.extractTokenValue()
+
+        try {
+            val username = tokenService.extractUsername(jwtToken)
+
+            if (username != null && SecurityContextHolder.getContext().authentication == null) {
+                val foundUser = userDetailsService.loadUserByUsername(username)
+
+                if (tokenService.isValid(jwtToken, foundUser)) {
+                    updateContext(foundUser, request)
+                } else {
+                    logAndRespond(response, "Invalid JWT token.")
+                    return
+                }
+            }
+        } catch (e: Exception) {
+            logAndRespond(response, "Invalid JWT token.")
             return
         }
-        val jwtToken = authHeader!!.extractTokenValue()
-        val username = tokenService.extractUsername(jwtToken)
-        if (username != null && SecurityContextHolder.getContext().authentication == null) {
-            val foundUser = userDetailsService.loadUserByUsername(username)
-            if (tokenService.isValid(jwtToken, foundUser))
-                updateContext(foundUser, request)
-            filterChain.doFilter(request, response)
-        }
+
+        filterChain.doFilter(request, response)
     }
 
     private fun String?.doesNotContainBearerToken() =
@@ -47,5 +63,12 @@ class JwtAuthenticationFilter(
         val authToken = UsernamePasswordAuthenticationToken(foundUser, null, foundUser.authorities)
         authToken.details = WebAuthenticationDetailsSource().buildDetails(request)
         SecurityContextHolder.getContext().authentication = authToken
+    }
+
+    private fun logAndRespond(response: HttpServletResponse, message: String) {
+        log.warn(message)
+        response.contentType = "application/json"
+        response.status = HttpServletResponse.SC_FORBIDDEN
+        response.writer.write("""{"error": "$message"}""")
     }
 }
